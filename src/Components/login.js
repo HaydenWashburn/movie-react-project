1be814cbd13bbe129d26515d871449f5dd73dc7c
HaydenWashburn <hayden.washburn1@gmail.com>
2023-05-22 15:24:10 -0500

    updated links

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,10 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom';
 import React from "react";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return  <a class="waves-effect waves-light btn red" onClick={() => loginWithRedirect()}>Log In</a>;
-};
-
+  return  <Link class="waves-effect waves-light btn red" onClick={() => loginWithRedirect()}>Log In</Link>
+}
 export default LoginButton;
